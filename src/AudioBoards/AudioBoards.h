125a6dde1149c1e14178a009f0e13fa16c3c5381
pschatzmann <phil.schatzmann@gmail.com>
2026-07-04 14:59:48 +0200

    include boards by platform

--- src/AudioBoards/AudioBoards.h
+++ src/AudioBoards/AudioBoards.h
@@ -1,5 +1,6 @@
 #pragma once
-// All boards using gpios as ints are automatically included in AudioBoard.h
+// All ESP32 boards using gpios as ints are automatically included in AudioBoard.h
+#if defined(ESP32)
 #include "AudioKitAC101.h"
 #include "AudioKitEs8388v1.h"
 #include "AudioKitEs8388v2.h"
@@ -8,3 +9,9 @@
 #include "LyratMini.h"
 #include "M5stackAtomEchoS3R.h"
 #include "ESP32S3AISmartSpeaker.h"
+#endif
+
+// Import STM32 boards
+#if defined(ARDUINO_ARCH_STM32) || defined(STM32)
+#include "STM32F411Disco.h"
+#endif
